9455ffbb97e03203dfd079a27f1c8e48c1caf171
DongMyeongLee <dongmyeong.lee22@gmail.com>
2019-12-04 22:40:20 +0900

    PostsRepository 추가

---
+++ src/test/java/com/me/sun/awsspring/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,53 @@
+package com.me.sun.awsspring.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() throws Exception {
+        //given
+        String title = "제목";
+        String content = "본문";
+        String author = "123123@gmail.com";
+
+        Posts post = Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+
+        postsRepository.save(post);
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts getPosts = postsList.get(0);
+        assertThat(getPosts.getTitle()).isEqualTo(title);
+        assertThat(getPosts.getContent()).isEqualTo(content);
+        assertThat(getPosts.getAuthor()).isEqualTo(author);
+    }
+
+
+}
