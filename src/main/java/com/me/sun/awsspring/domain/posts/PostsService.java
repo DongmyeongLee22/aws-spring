ac5cf46d57ab81e0cc22d78292bb7b479d9e191a
DongMyeongLee <dongmyeong.lee22@gmail.com>
2019-12-04 22:53:25 +0900

    Posts 저장 기능 추가

---
+++ src/main/java/com/me/sun/awsspring/domain/posts/PostsService.java
@@ -0,0 +1,15 @@
+package com.me.sun.awsspring.domain.posts;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/test/java/com/me/sun/awsspring/domain/posts/PostsApiControllerTest.java
@@ -0,0 +1,68 @@
+package com.me.sun.awsspring.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class PostsApiControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void Post등록() throws Exception {
+        //given
+        String title = "제목";
+        String content = "본문";
+        String author = "123123@gmail.com";
+
+        PostsSaveRequestDto requestDto = PostsSaveRequestDto.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts";
+
+        //when
+        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
+
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> postsList = postsRepository.findAll();
+        Posts getPosts = postsList.get(0);
+
+        assertThat(getPosts.getTitle()).isEqualTo(title);
+        assertThat(getPosts.getContent()).isEqualTo(content);
+        assertThat(getPosts.getAuthor()).isEqualTo(author);
+    }
+
+
+}
