6134e548b29181b8ab068f1380f9e3cd420c42a0
DongMyeongLee <dongmyeong.lee22@gmail.com>
2019-12-05 01:09:03 +0900

    게시글 수정 화면 추가

--- src/main/java/com/me/sun/awsspring/web/IndexController.java
+++ src/main/java/com/me/sun/awsspring/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.me.sun.awsspring.web;
 
 import com.me.sun.awsspring.domain.posts.PostsService;
+import com.me.sun.awsspring.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,6 +18,14 @@ public class IndexController {
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
         return "index";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 
     @GetMapping("/posts/save")
