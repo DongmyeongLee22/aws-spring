42db40082e3ff16d681b77bb54234d70e92d2589
DongMyeongLee <dongmyeong.lee22@gmail.com>
2019-12-04 23:28:28 +0900

    생성, 수정시간 자동화 Entity 추가

--- src/test/java/com/me/sun/awsspring/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/me/sun/awsspring/domain/posts/PostsRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -47,6 +48,32 @@ public class PostsRepositoryTest {
         assertThat(getPosts.getTitle()).isEqualTo(title);
         assertThat(getPosts.getContent()).isEqualTo(content);
         assertThat(getPosts.getAuthor()).isEqualTo(author);
+    }
+
+    @Test
+    public void BaseTimeEntity_테스트() throws Exception {
+        //given
+        LocalDateTime now = LocalDateTime.now();
+        Posts posts = Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build();
+        postsRepository.save(posts);
+
+        //when
+        List<Posts> all = postsRepository.findAll();
+
+        //then
+        Posts findPosts = all.get(0);
+
+        System.out.println("==============================");
+        System.out.println(findPosts.getCreatedDate());
+        System.out.println(findPosts.getModifiedDate());
+        System.out.println("==============================");
+
+        assertThat(findPosts.getCreatedDate()).isAfter(now);
+        assertThat(findPosts.getModifiedDate()).isAfter(now);
     }
 
 
